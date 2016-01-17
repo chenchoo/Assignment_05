bf889ac3d7f8af569eee73809d85c94313d74d83
chenchoo <Chen@Chens-MacBook-Pro.local>
2016-01-17 20:20:01 +1100

    Commit_01

---
+++ js/index.js
@@ -0,0 +1,54 @@
+$('document').ready(function(){
+
+
+    $('document').submit(function(e){
+      e.preventDefault();
+    });
+
+
+
+    $('.readmore').click(function(){
+
+      $('#show-this-on-click').slideDown("slow");
+
+      $('#readlessLink').show();
+
+      $('.readmore').hide();
+
+    });
+
+
+    $('#readlessLink').click(function(){
+
+      $('#show-this-on-click').slideUp("slow");
+
+      $('#readlessLink').hide();
+
+      $('.readmore').show('slow');
+
+    });
+
+
+    $('.learnmore').click(function(){
+
+      $('#learnmoretext').slideDown("slow");
+
+      $('.learnmore').hide();
+
+    });
+
+
+    //HOW TO STOP JUMPING UP THE PAGE WHEN LEARN MORE IS CLICKED?
+
+
+
+
+
+
+
+
+
+
+
+
+});
